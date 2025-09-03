c1907041df9f0443891f3208ff70979d37941793
Weberth Machado <weberth.machado@jrsistemas.net>
2025-09-03 18:30:37 -0300

    Adicionados mais testes automatizados de números zerados e numeros negativos para a função de soma

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -2,26 +2,30 @@ const { expect } = require('chai');
 const {somarDoisNumeros} = require('../src/calculadora');
 
 
-describe('Testes da função de soma', function(){  //Função do mocha (O describe representa um grupo de testes)
-    it('Somar dois numeros positivos', function(){ // Representa um teste específico
-       //Coleta o resultado da função
-        const resultadoSoma = somarDoisNumeros(7, 4);
+describe('Testes da função de soma', function(){            //Função do mocha (O describe representa um grupo de testes)
+    it('Somar dois numeros positivos', function(){          // Representa um teste específico
 
-        //compara o valor que você espera com o resultado da função
-        expect(resultadoSoma).to.equal(11); //chai para comparar 2 valores
-    });
-
-    it('Somar dois números negativos', function(){ // Representa um teste específico
-         const resultadoSoma = somarDoisNumeros(-7, -3);
-         expect(resultadoSoma).to.equal(-10);
+        const resultadoSoma = somarDoisNumeros(7, 4);  
+        expect(resultadoSoma).to.equal(11);                 //chai para comparar 2 valores
     });
 
     it('Somar um número positivo e um negativo', function(){ // Representa um teste específico
+         
          const resultadoSoma = somarDoisNumeros( 17, -3);
-         expect(resultadoSoma).to.equal(14);  //chai para comparar 2 valores
+         expect(resultadoSoma).to.equal(14);                //chai para comparar 2 valores
     });
 
+    it('Somar dois números negativos', function(){          // Representa um teste específico
+         const resultadoSoma = somarDoisNumeros(-7, -3);
+         expect(resultadoSoma).to.equal(-10);
+     });
     
+     it('Somar dois números zerados', function(){           // Representa um teste específico
+        // Coleta o resultado da função
+         const resultadoSoma = somarDoisNumeros(0, 0);
+         expect(resultadoSoma).to.equal(0);                 //chai para comparar 2 valores
+     });
+
 }); 
 
 
